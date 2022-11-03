d05e78134d4f46c06a28eb46ac8d371e5ee2b945
Martynezys <lenlein@proton.me>
2022-11-03 08:51:35 +0100

    add -- function that converts year to century.

--- Open-Lab-05.01/DateTools.cs
+++ Open-Lab-05.01/DateTools.cs
@@ -6,7 +6,49 @@ namespace Open_Lab_05._01
     {
         public string Century(int year)
         {
-            throw new NotImplementedException();
+
+            int rok = year;
+            string s;
+            int cent = year / 100;
+            year %= 100;
+           
+
+
+            if (year > 0)
+                cent = cent + 1;
+
+            s = cent.ToString();
+
+            if (cent == 11 || cent == 12 || cent == 13)
+            {
+                s = string.Format(cent + "th century");
+                return s;
+            }
+            else if (cent == 1 || cent % 10 == 1)
+            {
+                s = string.Format(cent + "st century");
+                return s;
+            }
+            else if (cent == 2 || cent % 10 == 2)
+            {
+                s = string.Format(cent + "nd century");
+                return s;
+            }
+            else if (cent == 3 || cent % 10 == 3)
+            {
+                s = string.Format(cent + "rd century");
+                return s;
+            }
+            else
+            {
+                s = string.Format(cent + "th century");
+                return s;
+            }
+            
+
+           
+            
+
         }
     }
 }
